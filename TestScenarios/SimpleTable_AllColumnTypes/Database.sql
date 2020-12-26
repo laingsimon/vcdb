83e7d748ec60c65e4a2685733d74ac9b4d511a75
Simon Laing <laingster@gmail.com>
2020-12-26 10:14:35 +0000

    Add uniqueidentifier column type

--- TestScenarios/SimpleTable_AllColumnTypes/Database.sql
+++ TestScenarios/SimpleTable_AllColumnTypes/Database.sql
@@ -8,11 +8,12 @@ USE [Scenario1]
 GO
 
 CREATE TABLE S1_T1 (
-	Id 		int	identity,
-	Name 	nvarchar(255) not null,
-	Title	varchar(10),
-	Age		int,
-	Price	decimal(18, 2),
-	DoB		date,
-	Deleted	bit not null default(0)
+	Id 			int	identity,
+	Name 		nvarchar(255) not null,
+	Title		varchar(10),
+	Age			int,
+	Price		decimal(18, 2),
+	DoB			date,
+	Deleted		bit not null default(0),
+	UniqueId	uniqueidentifier
 )
