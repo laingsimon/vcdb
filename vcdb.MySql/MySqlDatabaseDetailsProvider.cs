ed20e77211e4939e97141962fd66a86d38970d3f
Simon Laing <laingster@gmail.com>
2021-02-15 17:06:01 +0000

    Add - empty - MySql project

---
+++ vcdb.MySql/MySqlDatabaseDetailsProvider.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+using vcdb.CommandLine;
+
+namespace vcdb.MySql
+{
+    public class MySqlDatabaseDetailsProvider : IDatabaseDetailsProvider
+    {
+        private readonly Options options;
+
+        public MySqlDatabaseDetailsProvider(Options options)
+        {
+            this.options = options;
+        }
+
+        public string GetServerName()
+        {
+            var connectionStringBuilder = new MySqlConnectionStringBuilder(options.ConnectionString);
+            return connectionStringBuilder.Server;
+        }
+    }
+}
---
+++ vcdb.MySql/README.md
@@ -0,0 +1,5 @@
+﻿## vcdb.MySql
+
+This assembly contains the ability to read and script changes to a MySQL database.
+
+Build and run the TestFramework or the integration tests in the vcdb solution.
