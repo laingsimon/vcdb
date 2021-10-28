e1e650da0907eb6d123e73c9318fffa9aaf7cf03
Simon Laing <laingster@gmail.com>
2021-10-28 16:31:10 +0100

    #10: Add repository for fetching user details

--- vcdb.MySql/SchemaBuilding/MySqlUserRepository.cs
+++ vcdb.MySql/SchemaBuilding/MySqlUserRepository.cs
@@ -1,16 +1,70 @@
-﻿using System.Collections.Generic;
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
 using System.Data.Common;
+using System.Linq;
 using System.Threading.Tasks;
+using vcdb.CommandLine;
 using vcdb.Models;
+using vcdb.MySql.SchemaBuilding.Models;
 using vcdb.SchemaBuilding;
 
 namespace vcdb.MySql.SchemaBuilding
 {
     public class MySqlUserRepository : IUserRepository
     {
-        public Task<Dictionary<string, UserDetails>> GetUsers(DbConnection connection)
+        private readonly Options options;
+
+        public MySqlUserRepository(Options options)
         {
-            return Task.FromResult(new Dictionary<string, UserDetails>());
+            this.options = options;
+        }
+
+        public async Task<Dictionary<string, UserDetails>> GetUsers(DbConnection connection)
+        {
+            var users = await connection.QueryAsync<MySqlUserDetails>(
+                $@"select * from mysql.user",
+                new { databaseName = options.Database });
+
+            return users.ToDictionary(
+                GetUserName,
+                CreateUserDetails);
+        }
+
+        private static string GetUserName(MySqlUserDetails user)
+        {
+            return $"{user.User}@{user.Host}";
+        }
+
+        private static UserDetails CreateUserDetails(MySqlUserDetails user)
+        {
+            return new UserDetails
+            {
+                LoginName = null,
+                Type = GetUserType(user),
+                Enabled = GetEnabled(user),
+            };
+        }
+
+        private static UserType GetUserType(MySqlUserDetails user)
+        {
+            switch (user.SslType)
+            {
+                case MySqlSslTypeEnum.X509:
+                    return UserType.CertificateAuthority;
+                default:
+                    if (user.plugin == "mysql_native_password")
+                        return UserType.DatabaseAuthority;
+
+                    throw new NotSupportedException($"Unsure on the UserType for this user {GetUserName(user)}");
+            }
+        }
+
+        private static OptOut GetEnabled(MySqlUserDetails user)
+        {
+            return user.account_locked == MySqlBoolEnum.Y
+                ? OptOut.False
+                : OptOut.True;
         }
     }
 }
